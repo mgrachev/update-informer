5470217ee37f6836f7dea0c8528cc776c1d4cae0
Grachev Mikhail <work@mgrachev.com>
2021-12-10 17:10:11 +0300

    test: add tests for registries: crates.io and github (#9)

--- src/registry/crates.rs
+++ src/registry/crates.rs
@@ -3,7 +3,11 @@ use serde::Deserialize;
 use crate::registry::Registry;
 use crate::Error;
 
-static BASE_URL: &str = "https://crates.io./api/v1/crates";
+#[cfg(test)]
+use mockito;
+
+#[cfg(not(test))]
+const REGISTRY_URL: &str = "https://crates.io";
 
 #[derive(Deserialize)]
 struct Response {
@@ -17,9 +21,19 @@ struct Version {
 
 pub struct Crates;
 
+fn get_base_url() -> String {
+    #[cfg(not(test))]
+    let base_url = format!("{}/api/v1/crates", REGISTRY_URL);
+
+    #[cfg(test)]
+    let base_url = format!("{}/api/v1/crates", &mockito::server_url());
+
+    base_url
+}
+
 impl Registry for Crates {
     fn get_latest_version(pkg_name: &str) -> Result<Option<String>, Error> {
-        let url = format!("{}/{}/{}", BASE_URL, pkg_name, "versions");
+        let url = format!("{}/{}/versions", get_base_url(), pkg_name);
 
         let resp: Response = ureq::get(&url).call()?.into_json()?;
 
@@ -28,5 +42,57 @@ impl Registry for Crates {
         }
 
         Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::mock;
+    use std::fs;
+
+    const PKG_NAME: &str = "dotenv-linter";
+    const FIXTURES_PATH: &str = "tests/fixtures/registry/crates";
+
+    #[test]
+    fn failure_test() {
+        let mock_path = format!("/api/v1/crates/{}/versions", PKG_NAME);
+        let data = fs::read_to_string(format!("{}/not_found.json", FIXTURES_PATH))
+            .expect("read file to string");
+
+        let _mock = mock("GET", mock_path.as_str())
+            .with_status(404)
+            .with_header("Content-Type", "application/json; charset=utf-8")
+            .with_body(data.to_string())
+            .create();
+
+        let result = Crates::get_latest_version(PKG_NAME);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn success_test() {
+        let mock_path = format!("/api/v1/crates/{}/versions", PKG_NAME);
+        let data = fs::read_to_string(format!("{}/versions.json", FIXTURES_PATH))
+            .expect("read file to string");
+
+        let json: Response = serde_json::from_str(&data).expect("deserialize json");
+        let latest_version = json
+            .versions
+            .first()
+            .expect("get latest version")
+            .num
+            .clone();
+
+        let _mock = mock("GET", mock_path.as_str())
+            .with_status(200)
+            .with_header("Content-Type", "application/json; charset=utf-8")
+            .with_body(data.to_string())
+            .create();
+
+        let result = Crates::get_latest_version(PKG_NAME);
+
+        assert!(result.is_ok());
+        assert_eq!(result.expect("get result"), Some(latest_version));
     }
 }
