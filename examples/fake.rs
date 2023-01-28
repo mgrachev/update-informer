74da4af97ae2f7233c61128ca9297666ed56ea4d
Mikhail Grachev <work@mgrachev.com>
2023-01-28 15:43:50 +0300

    chore: add more examples
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

---
+++ examples/fake.rs
@@ -0,0 +1,27 @@
+use serde::de::DeserializeOwned;
+use std::time::Duration;
+use update_informer::{http_client::SendRequest, registry, Check};
+
+struct YourOwnHttpClient;
+
+impl SendRequest for YourOwnHttpClient {
+    fn get<T: DeserializeOwned>(
+        _url: &str,
+        _timeout: Duration,
+        _headers: Option<(&str, &str)>,
+    ) -> update_informer::Result<T> {
+        todo!()
+    }
+}
+
+fn main() {
+    let pkg_name = "update-informer";
+    let current_version = "0.1.0";
+
+    let informer = update_informer::fake(registry::Crates, pkg_name, current_version, "0.2.0")
+        .http_client(YourOwnHttpClient);
+
+    if let Ok(Some(new_version)) = informer.check_version() {
+        println!("A new release of {pkg_name} is available: v{current_version} -> {new_version}");
+    }
+}
