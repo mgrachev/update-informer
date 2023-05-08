368cb78554d8fbb3eebe45ff144da4e09f7ca02a
Mikhail Grachev <work@mgrachev.com>
2023-05-08 14:15:44 +0300

    refactor: get rid of `Option<(&str, &str)>` for `UndefinedHttpClient`
    Signed-off-by: Mikhail Grachev <work@mgrachev.com>

--- src/http_client/undefined.rs
+++ src/http_client/undefined.rs
@@ -1,15 +1,14 @@
-use crate::{http_client::HttpClient, Result};
+use crate::{
+    http_client::{HeaderMap, HttpClient},
+    Result,
+};
 use serde::de::DeserializeOwned;
 use std::time::Duration;
 
 pub struct UndefinedHttpClient;
 
 impl HttpClient for UndefinedHttpClient {
-    fn get<T: DeserializeOwned>(
-        _url: &str,
-        _timeout: Duration,
-        _headers: Option<(&str, &str)>,
-    ) -> Result<T> {
+    fn get<T: DeserializeOwned>(_url: &str, _timeout: Duration, _headers: HeaderMap) -> Result<T> {
         panic!("choose HTTP client (ureq or reqwest) or implement your own");
     }
 }
