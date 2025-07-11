8b7f052dc4c5873ac9d6693355094e381f5464ab
Stefan Holderbach <sholderbach@users.noreply.github.com>
2025-07-11 12:34:47 +0200

    chore: bump `ureq` to 3.0 (#169)

--- src/http_client/ureq.rs
+++ src/http_client/ureq.rs
@@ -4,18 +4,24 @@ use crate::{
 };
 use serde::de::DeserializeOwned;
 use std::time::Duration;
+use ureq::Agent;
 
 pub struct UreqHttpClient;
 
 impl HttpClient for UreqHttpClient {
     fn get<T: DeserializeOwned>(url: &str, timeout: Duration, headers: HeaderMap) -> Result<T> {
-        let mut req = ureq::agent().get(url).timeout(timeout);
+        let agent: Agent = Agent::config_builder()
+            .timeout_global(Some(timeout))
+            .build()
+            .into();
+
+        let mut req = agent.get(url);
 
         for (header, value) in headers {
-            req = req.set(header, value);
+            req = req.header(header, value);
         }
 
-        let json = req.call()?.into_json()?;
+        let json = req.call()?.body_mut().read_json()?;
 
         Ok(json)
     }
