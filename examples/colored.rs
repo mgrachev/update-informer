ca544bfadf52bec7bab131e70ee13e1003e296b0
Grachev Mikhail <work@mgrachev.com>
2025-07-11 18:42:15 +0300

    fix: fix a bug with ureq and native-tls feature (#172)

--- examples/colored.rs
+++ examples/colored.rs
@@ -1,11 +1,14 @@
 use colored::*;
+use std::time::Duration;
 use update_informer::{registry, Check};
 
 fn main() {
     let pkg_name = "update-informer";
     let current_version = "0.1.0";
 
-    let informer = update_informer::new(registry::Crates, pkg_name, current_version);
+    let informer =
+        update_informer::new(registry::Crates, pkg_name, current_version).interval(Duration::ZERO);
+
     if let Some(version) = informer.check_version().ok().flatten() {
         let msg = format!(
             "A new release of {pkg_name} is available: v{current_version} -> {new_version}",
